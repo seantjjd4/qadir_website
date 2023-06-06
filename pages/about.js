f07d4723d0b2fb883d218bd7312780c9ca608091
Sean <seanhung2000@gmail.com>
2023-06-06 18:15:16 +0800

    Update: about

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,46 @@
+import Image from 'next/image'
 import styles from '@/styles/About.module.css'
 
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
+import Homelink from '@/components/homelink'
 
 export default function About() {
     return (
         <>
             <Navbar/>
             <main className={styles.main}>
+                <Homelink/>
                 <div className={styles.title}>About Us</div>
                 <div className={styles.box}>
-                    <div className={styles.box_title}>
-                        About Qadir
+                    <div className={styles.box_image}>
+                        <Image
+                            src="/robot.png"
+                            alt="Robot Icon"
+                            width={100}
+                            height={100}
+                            priority
+                        />
                     </div>
-                    <div className={styles.box_description}>
-                        Qadir Corporation is at the forefront of advancing robotic automation through cutting-edge innovation.
-                        Our mission is to develop versatile and intelligent robotic systems that seamlessly integrate into various industries.
-                        By leveraging AI, machine learning, and robotics, we streamline processes, enhance productivity, and drive efficiency for our clients.
-                        Our goal is to make robotic automation an integral part of industries, transforming the way businesses operate.
+                    <div className={styles.box_text}>
+                        <div className={styles.box_title}>
+                            About QADIR
+                        </div>
+                        <div className={styles.box_description}>
+                            Qadir Corporation is at the forefront of advancing robotic automation through cutting-edge innovation.
+                            Our mission is to develop versatile and intelligent robotic systems that seamlessly integrate into various industries.
+                            By leveraging AI, machine learning, and robotics, we streamline processes, enhance productivity, and drive efficiency for our clients.
+                            Our goal is to make robotic automation an integral part of industries, transforming the way businesses operate.
+                        </div>
                     </div>
+                </div>
+                <div className={styles.robot_line}>
+                    <Image
+                        src="/robot_line.png"
+                        alt="Robot Working Line"
+                        fill={true}
+                        style={{ objectFit: 'cover' }}
+                    />
                 </div>
                 <div className={styles.box}>
                     <div className={styles.box_description}>
@@ -30,6 +52,14 @@ export default function About() {
                     <div className={styles.box_title}>
                         What We Do
                     </div>
+                </div>
+                <div className={styles.robot_line}>
+                    <Image
+                        src="/robot_line1.png"
+                        alt="Robot Working Line"
+                        fill={true}
+                        style={{ objectFit: 'cover' }}
+                    />
                 </div>
                 <div className={styles.box}>
                     <div className={styles.box_title}>
